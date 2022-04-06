2a4a6f5babed2c2a87a67122a1995b38e637ce83
mikeyolang <brunomike965@gmail.com>
2022-04-06 16:45:22 +0300

    React practice 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import { CardList } from './Components/card-list/card-list.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')  //fetching from the back end then returns a promise
+      .then(response => response.json()) //response in JSON file(Promise)
+      .then(users => this.setState({ monsters: users })) //The names gotten from the array are embeded to the monsters directly
+  }
+
+  render() {
+    return (
+      <div className='App'>
+            <CardList name="Mike"></CardList> 
+        {
+          this.state.monsters.map(monsters => (<h1 key={monsters.id}>{monsters.name}</h1>)) //Return every elemnt inn the array,
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/card-list/card-list.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+//Props have got children
+export const CardList = props => {
+    console.log(props);
+
+    return (
+        <div>Hello</div>
+    )
+}
